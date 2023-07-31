e202752343706a44786f83092a15dfdffd37bc6a
Misael Garcia <net.git.ketza>
2023-07-31 17:50:22 -0600

    Agregando el menú
    * Se ha agregado las opciones de listar, crear, seleccionar
    * Cuándo se ha seleccionado un archivo se puede escribir, eliminar, mostrar
    * Opción de salir
    * Se está mejorando la estructura para trabajar

--- cmd/manipulacion-csv/main.go
+++ cmd/manipulacion-csv/main.go
@@ -2,30 +2,78 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/g-code99/learning-path/week1/manipulacion-csv/pkg/utils"
 )
 
 func main() {
 	var in string
+	var inRead string
+	var fileName string
+	var inFile string
 
-	fmt.Printf("============ MENU ============\n" +
-		"1. Listar ficheros\n2. Eliminar fichero\n3. Seleccionar fichero\n4. Salir\n>>> ")
-
-	utils.ReadInput(&in)
-
-	switch in {
-	case "1":
-		fmt.Println("Listar ficheros")
-	case "2":
-		fmt.Println("")
-	case "3":
-		fmt.Println("")
-	case "4":
-		fmt.Println("")
-	default:
-		fmt.Println("")
+	for {
+		fmt.Printf("============ MENU ============\n" +
+			"1. Listar\n2. Crear\n3. Seleccionar\n4. Salir\n>>> ")
+		utils.ReadInput(&in)
+		switch in {
+		case "1":
+			fmt.Println("============ LISTANDO ============")
+			utils.ReadDir()
+			fmt.Println()
+		case "2":
+			fmt.Println("============ CREANDO ============")
+			fmt.Print("Ingresa el nombre del archivo: ")
+			utils.ReadInput(&inRead)
+			utils.CreateFile(inRead)
+			fmt.Println()
+		case "3":
+			fmt.Println("============ SELECCIONAR ============")
+			fmt.Println("Escribe el nombre del archivo")
+			fmt.Print(">>> ")
+			utils.ReadInput(&fileName)
+			if !utils.IsExists(fileName) {
+				continue
+			}
+		for_select:
+			for {
+				fmt.Printf("============ " + fileName + " ============\n" +
+					"1. Leer\n2. Eliminar\n3. Escribir\n4. Salir\n>>> ")
+				utils.ReadInput(&inRead)
+				switch inRead {
+				case "1":
+					fmt.Println("============ LEER ============")
+					utils.ReadFile(fileName)
+					fmt.Println()
+				case "2":
+					fmt.Println("============ ELIMINAR ============")
+					fmt.Println("¿Estás seguro que deseas eliminar el archivo? s/n")
+					utils.ReadInput(&inFile)
+					if inFile == "s" {
+						os.Remove("./data/" + fileName)
+						break for_select
+					}
+					continue
+				case "3":
+					fmt.Println("============ ESCRIBIR ============")
+					fmt.Println()
+				case "4":
+					fmt.Println("Salir...")
+					fileName = ""
+					inRead = ""
+					break for_select
+				default:
+					fmt.Println("Opción incorrecta. Vuelva a intentar.")
+				}
+			}
+			fmt.Println()
+		case "4":
+			fmt.Println("Saliendo...")
+			os.Exit(1)
+		default:
+			fmt.Println("Opción incorrecta. Vuelva a intentar.")
+			fmt.Println()
+		}
 	}
-
-	fmt.Println(in)
 }
--- pkg/utils/files.go
+++ pkg/utils/files.go
@@ -1,13 +1,53 @@
 package utils
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/olekukonko/tablewriter"
 )
 
 func ReadFile(fileName string) {
-	table, _ := tablewriter.NewCSV(os.Stdout, ("testdata/" + fileName + ".csv"), true)
-
+	table, _ := tablewriter.NewCSV(os.Stdout, ("./data/" + fileName + ".csv"), true)
 	table.Render()
+}
+
+// Leer los ficheros de un directorio
+func ReadDir() {
+	dir, err := os.ReadDir("./data")
+	if os.IsExist(err) {
+		log.Println("Doesn't exists directory data")
+	}
+	if err != nil {
+		log.Println("Something has occured:", err)
+	}
+	for index, directory := range dir {
+		fmt.Printf("%d. %s\n", (index + 1), directory.Name())
+	}
+}
+
+func IsExists(fileName string) bool {
+	_, err := os.Stat("./data/" + fileName + ".csv")
+	if os.IsNotExist(err) {
+		log.Println("File doesn't exists")
+		return false
+	}
+	if err != nil {
+		log.Println("Something has occured")
+		return false
+	}
+	return true
+}
+
+func CreateFile(fileName string) {
+	if len(fileName) == 0 {
+		log.Println("Check the name")
+	}
+	file, err := os.Create("./data/" + fileName + ".csv")
+	if err != nil {
+		log.Println("Cannot create file")
+	}
+	defer file.Close()
+	log.Println("The file:", file.Name(), "has been created.")
 }
